2bfc46a81677c32ca1bc5dab0b2133314a9dd602
Messy Toilet <jacob.chiu168@hotmail.com>
2023-04-11 02:09:42 -0400

    wireframe

--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,18 @@
 
 
 def main():
+    
     from Frontend import printLogo
     from instagrapi import Client
     from getpass import getpass
     import argparse        
     import time       
     import sys
+    import os
     
-    #---# init program: display logo
+    #---# init program: clear screen, display logo
 
+    os.system("cls")
     printLogo("green")
 
     #---# init user: grab login and login to account
@@ -18,16 +21,34 @@ def main():
 
     while True:
         try:
-            username = str(input("EMAIL: "))
+            username = str(input("USERNAME: "))
             password = getpass("PASSWORD: ")
-
             cl.login(username, password)
-            print(f"Login Successful")
+            print(f"\nLogin Successful")
+            time.sleep(2)
+            os.system("cls")
+            printLogo("green")
             break 
         except:
+            os.system("cls")
+            printLogo("green")
             print(f"\nERROR: could not login\n")
             pass
 
+    #---# get target user
+
+    while True:
+        os.system("cls")
+        printLogo("green")
+
+        target_user_id = cl.user_id_from_username(str(input(f"TARGET USERNAME: ")))
+ 
+        print(f"\nUSER INFO:\n")
+        print("\tUSER ID:", target_user_id)
+        print("\tFOLLOWING:", len(cl.user_following(target_user_id, 10)))
+        print("\tFOLLOWING:", len(cl.user_following_gql(target_user_id, 10)))
+        print("\tFOLLOWERS:", len(cl.user_followers(target_user_id, 10)))
+        print("\tFOLLOWERS:", len(cl.user_followers_gql(target_user_id, 10)))
 
     return
 
---
+++ Code/test.py
@@ -0,0 +1,16 @@
+import os
+
+# Clear the screen
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Print some output
+print("Hello, world!")
+
+# Wait for user input
+input("Press Enter to continue...")
+
+# Clear the screen again
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Print some more output
+print("Goodbye, world!")

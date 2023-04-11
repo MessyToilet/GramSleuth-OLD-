34c993fc736dae0fa5337b1053a4bdd62e8f88e5
Messy Toilet <jacob.chiu168@hotmail.com>
2023-04-11 01:10:55 -0400

    login working

--- Code/main.py
+++ Code/main.py
@@ -1,53 +1,33 @@
 
-def pingInstagram(proxy_addr):
-    import socket
-    import socks
-
-    socks.set_default_proxy(socks.SOCKS5, proxy_addr[0], proxy_addr[1])
-    socket.socket = socks.socksocket
-    
-    try:
-        # connect to Instagram
-        with socket.create_connection(("https://www.instagram.com/", 80), timeout=10) as conn:
-            pass
-        print("Connected to Instagram")
-        return True
-    except OSError:
-        pass
-    print("Cannot connect to Instagram")
-    return False
-
-
-
-
-def startUpFail():
-    import os
-    from tqdm import tqdm
-    import time
-
-    CWD = os.getcwd()
-    print(f"\ncurrent working directory: {CWD}")
-    
-    #check if needed files exist
-    if not os.path.exists(os.path.join(CWD, "CLIside.py")) or not os.path.exists(os.path.join(CWD, "INSTAside.py")):
-        print("ERROR: missing file(s)")
-        return True
-    else:
-        print("ALL FILES EXIST :)")
-
-    pingInstagram(("127.0.0.1", 30235))
-
-
 
 def main():
     from Frontend import printLogo
+    from instagrapi import Client
+    from getpass import getpass
     import argparse        
     import time       
+    import sys
     
-    if startUpFail():
-        return
+    #---# init program: display logo
 
     printLogo("green")
+
+    #---# init user: grab login and login to account
+
+    cl = Client()
+
+    while True:
+        try:
+            username = str(input("EMAIL: "))
+            password = getpass("PASSWORD: ")
+
+            cl.login(username, password)
+            print(f"Login Successful")
+            break 
+        except:
+            print(f"\nERROR: could not login\n")
+            pass
+
 
     return
 

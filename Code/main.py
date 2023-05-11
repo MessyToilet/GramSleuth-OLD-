af3c7e4447565d8c621723679d1c0bfe6a82b724
Messy Toilet <jacob.chiu168@hotmail.com>
2023-05-11 16:13:23 -0400

    updates

--- Code/main.py
+++ Code/main.py
@@ -1,18 +1,46 @@
 from instagramapi import *
+from Frontend import printLogo
 import time
 import sys
 
 init("GramSleuth\\requirments\\chromedriver.exe")
 
 #---#   START login seqeunce 
+printLogo("green")
+
 Uname = str(input("Username: "))
 Upass = str(input("Password: "))
 
 login(Uname, Upass)
+goHome()
 #---#   END login sequence
 
 goProfile()
 
-if str(input("Quit (y/n): ")).upper() == "Y":
-    quit()
-    sys.exit()
+
+while True:
+    choice = str(input("\n1. Go Home\t\t4. Go Reels\t\t7. Go Create\n2. Go Search\t\t5. Go Messages\t\t6. Go Notifications\n3. Go Explore\t\t8. Go Profile\t\t9. Go More\n10. test (get followers) \t11. Quit (Q)\n\n"))
+    if choice == '1':
+        goHome()
+    if choice == '2':
+        goSearch()
+    if choice == '3':
+        goExplore()
+    if choice == '4':
+        goReels()
+    if choice == '5':
+        goMessages()
+    if choice == '6':
+        goNotifications()
+    if choice == '7':
+        goCreate()
+    if choice == '8':
+        goProfile()
+    if choice == '9':
+        goMore()
+    if choice == '10':
+        getFollowers()
+
+    if choice.upper() == "Q":
+        quit()
+        sys.exit()
